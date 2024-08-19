95bba03ba4a06f73327b1997d1524d1f8620563f
turncoda <144861042+turncoda@users.noreply.github.com>
2024-08-18 18:55:24 -0700

    Add SetMood

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 set game_folder=C:\Program Files (x86)\Steam\steamapps\common\Pseudoregalia\pseudoregalia
-set pak_name=tpgm_entrypoint_v004_p.pak
+set pak_name=tpgm_entrypoint_v005_p.pak
 
 make_pak.py --asset-list asset_list.txt --output "%game_folder%\Content\Paks\%pak_name%"
 REM call "%game_folder%\Binaries\Win64\pseudoregalia-Win64-Shipping.exe"
